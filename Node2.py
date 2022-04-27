e4bf211e43987e2fd9276386efad199edc2317bb
weiyi900809 <71805848+weiyi900809@users.noreply.github.com>
2022-04-27 11:11:28 +0800

    Add files via upload

--- Node2.py
+++ Node2.py
@@ -1,45 +1,14 @@
 import time
 #from MyOwnPeer2PeerNode import MyOwnPeer2PeerNode
-from p2pnetwork.node import Node
 
-class MyOwnPeer2PeerNode (Node):
 
-    # Python class constructor
-	def __init__(self, host, port, id=None, callback=None, max_connections=0):
-		super(MyOwnPeer2PeerNode, self).__init__(host, port, id, callback, max_connections)
-		print("MyPeer2PeerNode: Started")
-	def node_message(self, node, data):
-        	print("node_message (" + self.id + ") from " + node.id + ": " + str(data))    	
-    
+from P2PNode import P2PNode
 
-    # all the methods below are called when things happen in the network.
-    # implement your network node behavior to create the required functionality.
-'''
-	def outbound_node_connected(self, node):
-		print("outbound_node_connected (" + self.id + "): " + node.id)
-		
-	def inbound_node_connected(self, node):
-		print("inbound_node_connected: (" + self.id + "): " + node.id)
-
-	def inbound_node_disconnected(self, node):
-		print("inbound_node_disconnected: (" + self.id + "): " + node.id)
-
-	def outbound_node_disconnected(self, node):
-		print("outbound_node_disconnected: (" + self.id + "): " + node.id)
-	
-	    
-   
-	def node_disconnect_with_outbound_node(self, node):
-		print("node wants to disconnect with oher outbound node: (" + self.id + "): " + node.id)
-		
-	def node_request_to_stop(self):
-		print("node is requested to stop (" + self.id + "): ")
-		'''
 
 
 # The main node that is able to make connections to other nodes
 # and accept connections from other nodes on port 8001.
-node = MyOwnPeer2PeerNode("10.0.2.15", 8002, 2)
+node = P2PNode("123.240.196.78", 8002, 2)
 
 # Do not forget to start it, it spins off a new thread!
 node.start()
