f143ea27995ad2686402874c28e54aace5b812df
weiyi900809 <71805848+weiyi900809@users.noreply.github.com>
2022-04-27 17:04:36 +0800

    Add files via upload

--- Node_recv.py
+++ Node_recv.py
@@ -10,12 +10,12 @@ node = P2PNode("123.240.196.78", 10001, ID)
 # Do not forget to start it, it spins off a new thread!
 node.start()
 
-print('waiting 30 sec for connecting')
+print('waiting 10 sec for connecting')
 
 
-time.sleep(30)
+time.sleep(10)
 print('Time to send message')
-node.send_to_nodes('{"message": "hi from node node_recv"}') 
-
+node.send_to_nodes('{"message": "hi from node recv"}') 
+time.sleep(5)
 # Gracefully stop the node.
 node.stop()
--- P2PNode.py
+++ P2PNode.py
@@ -6,7 +6,8 @@
 # 28/06/2021: Added the new developments on id and max_connections
 #######################################################################################################################
 from p2pnetwork.node import Node
-
+import time
+import socket 
 class P2PNode (Node):
 
     # Python class constructor
@@ -21,7 +22,9 @@ class P2PNode (Node):
         print("outbound_node_connected (" + self.id + "): " + node.id+ " ip: " + node.host + " port: " + str(node.port))
         
     def inbound_node_connected(self, node):
-        self.connect_with_node(node.host, node.port) #如果有node連接我,我會返回去連接對方
+        
+        #self.connect_with_node(node.host, node.port) #如果有node連接我,我會返回去連接對方
+      
         print("inbound_node_connected: (" + self.id + "): " + node.id+ " ip: " + node.host + " port: " + str(node.port))
 
     def inbound_node_disconnected(self, node):
@@ -39,4 +42,62 @@ class P2PNode (Node):
     def node_request_to_stop(self):
         print("node is requested to stop (" + self.id + "): ")
         
-    
+    def run(self):
+        """The main loop of the thread that deals with connections from other nodes on the network. When a
+           node is connected it will exchange the node id's. First we receive the id of the connected node
+           and secondly we will send our node id to the connected node. When connected the method
+           inbound_node_connected is invoked."""
+        while not self.terminate_flag.is_set():  # Check whether the thread needs to be closed
+            try:
+                self.debug_print("Node: Wait for incoming connection")
+                connection, client_address = self.sock.accept()
+
+                self.debug_print("Total inbound connections:" + str(len(self.nodes_inbound)))
+                # When the maximum connections is reached, it disconnects the connection 
+                if self.max_connections == 0 or len(self.nodes_inbound) < self.max_connections:
+                    
+                    # Basic information exchange (not secure) of the id's of the nodes!
+                    connected_node_port = client_address[1] # backward compatibilty
+                    connected_node_id   = connection.recv(4096).decode('utf-8')
+                    if ":" in connected_node_id:
+                        (connected_node_id, connected_node_port) = connected_node_id.split(':') # When a node is connected, it sends it id!
+                    connection.send(self.id.encode('utf-8')) # Send my id to the connected node!
+
+                    thread_client = self.create_new_connection(connection, connected_node_id, client_address[0], connected_node_port)
+                    thread_client.start()
+                    self.connect_with_node(thread_client.host,thread_client.port)
+                    self.nodes_inbound.append(thread_client)
+                    self.inbound_node_connected(thread_client)
+
+                else:
+                    self.debug_print("New connection is closed. You have reached the maximum connection limit!")
+                    connection.close()
+            
+            except socket.timeout:
+                self.debug_print('Node: Connection timeout!')
+
+            except Exception as e:
+                raise e
+
+            self.reconnect_nodes()
+
+            time.sleep(0.01)
+
+        print("Node stopping...")
+        for t in self.nodes_inbound:
+            t.stop()
+
+        for t in self.nodes_outbound:
+            t.stop()
+
+        time.sleep(1)
+
+        for t in self.nodes_inbound:
+            t.join()
+
+        for t in self.nodes_outbound:
+            t.join()
+
+        self.sock.settimeout(None)   
+        self.sock.close()
+        print("Node stopped")
